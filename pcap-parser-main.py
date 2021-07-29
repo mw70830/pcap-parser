825a218038f1dffa0fb3e72cb5ccb386db6d80ed
Tae-seon, Hwang <mw70830@gmail.com>
2021-07-29 13:42:42 +0900

    ADD arguments handling
    changes:
        * add arguments handling

--- pcap-parser-main.py
+++ pcap-parser-main.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-def start_program():
+import argparse
+import os
+
+def start_program(path):
+    print(type(path), path)
     pass
 
+def arguments_parse():
+    parser = argparse.ArgumentParser(description='TCP analysis from pcap file.')
+    parser.add_argument("-p", required=True, nargs='+', dest='paths',
+                        help='target pcap file paths(use Absolute path)')
+    args = parser.parse_args()
+
+    for path in args.paths:
+        if os.path.exists(path) == True:
+            start_program(path)
+        else:
+            print("This file or path is not exists.", path)
+
 if __name__ == "__main__":
-    start_program()
+    arguments_parse()
